6bdc54b2c3e8c62a9c9c542f0402fb85fc680eae
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2025-04-16 09:49:47 +0600

    Create 10. Fibonacci & Factorial.c

---
+++ 10. Fibonacci & Factorial.c
@@ -0,0 +1,17 @@
+Problem: Fibonacci Series
+IntFib(int n)
+{
+IntA[];
+A[0] =0; A[1] = 1;
+For (i=2; i<=n;i++){
+A[i]= A[i-1] + A[i-2];
+Return A[n];
+}
+Problem: Factorial of a Number 
+intFactorial (int n){
+Fact[0] =1;
+For (i=2; i<=n;i++){
+Fact[i] = fact[i-1] *i ;
+} 
+Return Fact[n];
+}
